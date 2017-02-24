935ca05e2a3cdc3bd12205202fe1132203fdc10e
Michael Schennum <mikeschenphoto@gmail.com>
2017-02-24 13:55:59 -0700

    adds edit and create

--- ngApp/services/services.ts
+++ ngApp/services/services.ts
@@ -1,3 +1,23 @@
 namespace productsapp.Services {
+  export class ProductService {
+    private ProductResource;
 
+    public get(id) {
+      return this.ProductResource.get({id:id});
     }
+
+    public list() {
+      return this.ProductResource.query()
+    }
+
+    public save(product) {
+      return this.ProductResource.save(product).$promise;
+    }
+
+    constructor($resource:ng.resource.IResourceService) {
+      this.ProductResource = $resource('/api/products/:id');
+    }
+  }
+
+  angular.module('productsapp').service('productService', ProductService);
+}
